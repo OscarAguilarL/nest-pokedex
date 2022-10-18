343e6697b645a486e7ce170df61987fc40dcbd71
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-10-18 10:29:48 -0500

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const apiPath = 'api/v2';
 
-  app.setGlobalPrefix('api/v2');
+  const config = new DocumentBuilder()
+    .setTitle('Pokedex')
+    .setDescription('Pokedex API description')
+    .setVersion('2.0')
+    .addTag('pokemons')
+    .addServer('v2')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(apiPath, app, document);
+
+  app.setGlobalPrefix(apiPath);
 
   app.useGlobalPipes(
     new ValidationPipe({
